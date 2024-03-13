3aed208da3cc40172a543b8481997596bec6b8e3
Pablo Mariano <pablomariano@gmail.com>
2024-03-13 15:08:40 -0300

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\StateSeeder;
+use Database\Seeders\CountrySeeder;
+use Database\Seeders\CitySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +18,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+  /*       User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]); */
+        $this->call(CountrySeeder::class);
+        $this->call(StateSeeder::class);
+        //$this->call(CitySeeder::class);
+        
     }
 }
