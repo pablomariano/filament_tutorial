ee0193a747d9441d04cd0e2ddbfea6c0dd8be43e
Pablo Mariano <pablomariano@gmail.com>
2024-03-13 17:03:49 -0300

    edit create employee form

--- app/Filament/Resources/CityResource.php
+++ app/Filament/Resources/CityResource.php
@@ -31,9 +31,12 @@ class CityResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('state_id')
-                    ->required()
-                    ->numeric(),
+                Forms\Components\Select::make('state_id')
+                    ->relationship(name: 'state', titleAttribute: 'name')
+                    ->searchable(true)
+                    ->preload()
+                    ->native(true)
+                    ->required(),
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
